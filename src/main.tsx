986170c7a7f5f1c483e7d33286cd2b9a2e2df3cf
stardustrain <kthanterran@gmail.com>
2026-06-10 17:27:26 +0900

    feat: apply github-markdown-css and app chrome styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "github-markdown-css/github-markdown.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
